5f37d18f90bb053d1bbd84ee5f02bcb4c89e3706
Diganta Kalita <47485188+realdiganta@users.noreply.github.com>
2019-11-29 00:56:37 +0530

    Update rl_gridworld.py

--- rl_gridworld.py
+++ rl_gridworld.py
@@ -19,10 +19,10 @@ for num in range(10): #number of times we will go through the whole grid
   for i in range(5):      #all the rows
     for j in range(5):    #all the columns
       
-      up_grid = grid[i-1][j] if i > 0 else 0   #if going up takes us out of the grid then its value be 0
-      down_grid = grid[i+1][j] if i < 4 else 0  #if going down takes us out of the grid then its value be 0
-      left_grid = grid[i][j-1] if j > 0 else 0  #if going left takes us out of the grid then its value be 0
-      right_grid = grid[i][j+1] if j < 4 else 0  #if going right takes us out of the grid then its value be 0
+      up_grid = grid[i-1][j] if i > 0 else None   #if going up takes us out of the grid then its value be 0
+      down_grid = grid[i+1][j] if i < 4 else None  #if going down takes us out of the grid then its value be 0
+      left_grid = grid[i][j-1] if j > 0 else None  #if going left takes us out of the grid then its value be 0
+      right_grid = grid[i][j+1] if j < 4 else None  #if going right takes us out of the grid then its value be 0
 
       all_dirs = [up_grid, down_grid, left_grid, right_grid]     
 
@@ -33,7 +33,7 @@ for num in range(10): #number of times we will go through the whole grid
         value = 5 + Y*grid[2][3]
       else:
         for direc in all_dirs:
-          if direc != 0: 
+          if direc != None: 
             value += .25 * (0 + Y*direc)  #if we don't go out of the grid
           else:
             value += .25 * (-1 + Y*grid[i][j]) #if we go out of the grid
